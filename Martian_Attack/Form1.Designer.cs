b45819dc5e5c36a4eb422627718211a975cde0ca
Phillip <phillipyates12@hotmail.com>
2020-12-14 12:44:30 -0800

    Added MainPlayer Info and PNG

--- Martian_Attack/Form1.Designer.cs
+++ Martian_Attack/Form1.Designer.cs
@@ -29,21 +29,39 @@ namespace Martian_Attack
         /// </summary>
         private void InitializeComponent()
         {
+            this.MainPlayer = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainPlayer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // MainPlayer
+            // 
+            this.MainPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.MainPlayer.Image = global::Martian_Attack.Properties.Resources.PhotoshopShip;
+            this.MainPlayer.Location = new System.Drawing.Point(283, 234);
+            this.MainPlayer.Name = "MainPlayer";
+            this.MainPlayer.Size = new System.Drawing.Size(80, 59);
+            this.MainPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.MainPlayer.TabIndex = 0;
+            this.MainPlayer.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.MainPlayer);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "Form1";
             this.Text = "Martian Attack";
+            ((System.ComponentModel.ISupportInitialize)(this.MainPlayer)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox MainPlayer;
     }
 }
 
