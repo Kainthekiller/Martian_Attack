60b79be27cb3bbd04f9bff2c106b8502343feef1
Phillip <phillipyates12@hotmail.com>
2020-12-14 13:27:06 -0800

    Added Stars, Added Score Labe, Added GameTimer

--- Martian_Attack/Form1.Designer.cs
+++ Martian_Attack/Form1.Designer.cs
@@ -29,9 +29,42 @@ namespace Martian_Attack
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.Star1 = new System.Windows.Forms.PictureBox();
             this.MainPlayer = new System.Windows.Forms.PictureBox();
+            this.Star2 = new System.Windows.Forms.PictureBox();
+            this.Star3 = new System.Windows.Forms.PictureBox();
+            this.Star4 = new System.Windows.Forms.PictureBox();
+            this.Star5 = new System.Windows.Forms.PictureBox();
+            this.Star6 = new System.Windows.Forms.PictureBox();
+            this.Star7 = new System.Windows.Forms.PictureBox();
+            this.Star8 = new System.Windows.Forms.PictureBox();
+            this.Star9 = new System.Windows.Forms.PictureBox();
+            this.Star10 = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Star1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.MainPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star10)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Star1
+            // 
+            this.Star1.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star1.Location = new System.Drawing.Point(90, 234);
+            this.Star1.Name = "Star1";
+            this.Star1.Size = new System.Drawing.Size(60, 37);
+            this.Star1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star1.TabIndex = 1;
+            this.Star1.TabStop = false;
             // 
             // MainPlayer
             // 
@@ -44,24 +77,164 @@ namespace Martian_Attack
             this.MainPlayer.TabIndex = 0;
             this.MainPlayer.TabStop = false;
             // 
+            // Star2
+            // 
+            this.Star2.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star2.Location = new System.Drawing.Point(479, 234);
+            this.Star2.Name = "Star2";
+            this.Star2.Size = new System.Drawing.Size(60, 37);
+            this.Star2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star2.TabIndex = 2;
+            this.Star2.TabStop = false;
+            // 
+            // Star3
+            // 
+            this.Star3.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star3.Location = new System.Drawing.Point(594, 32);
+            this.Star3.Name = "Star3";
+            this.Star3.Size = new System.Drawing.Size(60, 37);
+            this.Star3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star3.TabIndex = 3;
+            this.Star3.TabStop = false;
+            // 
+            // Star4
+            // 
+            this.Star4.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star4.Location = new System.Drawing.Point(368, 43);
+            this.Star4.Name = "Star4";
+            this.Star4.Size = new System.Drawing.Size(60, 37);
+            this.Star4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star4.TabIndex = 4;
+            this.Star4.TabStop = false;
+            // 
+            // Star5
+            // 
+            this.Star5.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star5.Location = new System.Drawing.Point(801, 86);
+            this.Star5.Name = "Star5";
+            this.Star5.Size = new System.Drawing.Size(60, 37);
+            this.Star5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star5.TabIndex = 5;
+            this.Star5.TabStop = false;
+            // 
+            // Star6
+            // 
+            this.Star6.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star6.Location = new System.Drawing.Point(910, 245);
+            this.Star6.Name = "Star6";
+            this.Star6.Size = new System.Drawing.Size(60, 37);
+            this.Star6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star6.TabIndex = 6;
+            this.Star6.TabStop = false;
+            // 
+            // Star7
+            // 
+            this.Star7.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star7.Location = new System.Drawing.Point(1034, 32);
+            this.Star7.Name = "Star7";
+            this.Star7.Size = new System.Drawing.Size(60, 37);
+            this.Star7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star7.TabIndex = 7;
+            this.Star7.TabStop = false;
+            // 
+            // Star8
+            // 
+            this.Star8.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star8.Location = new System.Drawing.Point(1241, 188);
+            this.Star8.Name = "Star8";
+            this.Star8.Size = new System.Drawing.Size(60, 37);
+            this.Star8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star8.TabIndex = 8;
+            this.Star8.TabStop = false;
+            // 
+            // Star9
+            // 
+            this.Star9.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star9.Location = new System.Drawing.Point(1221, 64);
+            this.Star9.Name = "Star9";
+            this.Star9.Size = new System.Drawing.Size(60, 37);
+            this.Star9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star9.TabIndex = 9;
+            this.Star9.TabStop = false;
+            // 
+            // Star10
+            // 
+            this.Star10.Image = global::Martian_Attack.Properties.Resources.Star;
+            this.Star10.Location = new System.Drawing.Point(668, 170);
+            this.Star10.Name = "Star10";
+            this.Star10.Size = new System.Drawing.Size(60, 37);
+            this.Star10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Star10.TabIndex = 10;
+            this.Star10.TabStop = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.Font = new System.Drawing.Font("Ravie", 19.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ScoreLabel.Location = new System.Drawing.Point(1, 9);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(319, 71);
+            this.ScoreLabel.TabIndex = 11;
+            this.ScoreLabel.Text = "Score: 0";
+            this.ScoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.Star10);
+            this.Controls.Add(this.Star9);
+            this.Controls.Add(this.Star8);
+            this.Controls.Add(this.Star7);
+            this.Controls.Add(this.Star6);
+            this.Controls.Add(this.Star5);
+            this.Controls.Add(this.Star4);
+            this.Controls.Add(this.Star3);
+            this.Controls.Add(this.Star2);
+            this.Controls.Add(this.Star1);
             this.Controls.Add(this.MainPlayer);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
             this.Text = "Martian Attack";
+            ((System.ComponentModel.ISupportInitialize)(this.Star1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.MainPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star10)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox MainPlayer;
+        private System.Windows.Forms.PictureBox Star1;
+        private System.Windows.Forms.PictureBox Star2;
+        private System.Windows.Forms.PictureBox Star3;
+        private System.Windows.Forms.PictureBox Star4;
+        private System.Windows.Forms.PictureBox Star5;
+        private System.Windows.Forms.PictureBox Star6;
+        private System.Windows.Forms.PictureBox Star7;
+        private System.Windows.Forms.PictureBox Star8;
+        private System.Windows.Forms.PictureBox Star9;
+        private System.Windows.Forms.PictureBox Star10;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Timer GameTimer;
     }
 }
 
--- Martian_Attack/Form1.cs
+++ Martian_Attack/Form1.cs
@@ -16,5 +16,10 @@ namespace Martian_Attack
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
